efea0ddb970a82dc14f5c1f48cce9030d1c6df9e
Dr. Aidoo <aidoojuniorsolomon@gmail.com>
2021-01-07 05:07:20 -0800

    Update

---
+++ FlutterUIChallenge/lib/Day1_LoginPage/loginUI.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(LoginUI());
+}
+
+class LoginUI extends StatefulWidget {
+  LoginUI({Key key}) : super(key: key);
+
+  @override
+  _LoginUIState createState() => _LoginUIState();
+}
+
+class _LoginUIState extends State<LoginUI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
